71965770a9b8a33f5057f1b1375c22e922684873
PHUC321421 <46537465+PHUC321421@users.noreply.github.com>
2019-01-17 12:19:41 -0600

    update team strategy

--- team1.py
+++ team1.py
@@ -26,10 +26,11 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     if len(my_history)==0:
-        if (my_history)%2:
-            return 'c'
-        else:
-            return 'b'
+        return 'b'
+    if len(my_history)%2 == 0:
+        return 'c'
+    else:
+        return 'b'
     
         
     
