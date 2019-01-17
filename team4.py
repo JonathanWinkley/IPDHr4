725255778ec6341ff1ce058a5f6d3d50f589f191
amanP321 <46537428+amanP321@users.noreply.github.com>
2019-01-17 12:17:37 -0600

    Updated team strat
    has strat in it

--- team4.py
+++ team4.py
@@ -17,7 +17,20 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if len(my_history) <3:
+        return 'b'
+    if their_history[-1] == 'c' and their_history[-2] == 'c' and their_history[-3] == 'c':
+        return 'b'
+        if their_history[-1] == 'b':
+            return 'c'
+    if their_history[-1] == 'b' and their_history[-2] == 'b' and their_history[-3] == 'b':
+        return 'b'
+    
+            
+            
+    
+        
+    if 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +39,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
